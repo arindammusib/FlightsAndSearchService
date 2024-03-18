729692fb883573ce46748a1730152de61697a8d4
Arindam <amusib32@gmail.com>
2024-03-19 00:53:16 +0530

    set up server and middlewares and env

---
+++ README.md
@@ -0,0 +1,14 @@
+Flights And Search Service
+- src
+  - config
+  - controllers
+  - middlewares
+  - models
+  - respository
+  - utils
+
+- setUp express server🌏
+- set up env
+- set up middlewares(bodyparser)
+
+
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+const{PORT}=require('./config/serverConfig');
+const bodyParser=require('body-parser');
+
+const setUpAndStartServer=async ()=>{
+    const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.listen(PORT,()=>{
+        console.log(`server is running at ${PORT}`);
+       
+    });
+
+}
+setUpAndStartServer();
